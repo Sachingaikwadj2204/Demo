97c252d66d98275188a6abdc3f32302fa882ff8f
Sachingaikwadj2204 <sachingaikwadj2204@gmail.com>
2024-08-01 11:02:55 +0530

    Update Demo.java

--- gitDemo/src/gitDemo/Demo.java
+++ gitDemo/src/gitDemo/Demo.java
@@ -10,7 +10,12 @@ public class Demo {
 	System.out.println("this is demo m1");
 		
 	}
-	
+
+	public void m2(){
+
+	System.out.println("this is demo m2");
+		
+	}
 	public static void main(String[] args) {
 		
 		
